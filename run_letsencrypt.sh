5f90e29308ce341f288833c3305ff2ea33520009
Joeri van Dooren <joeri@weepee.org>
2016-01-16 00:53:26 +0100

    fix

--- run_letsencrypt.sh
+++ run_letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 cd /var/ssl
 /root/letsencrypt.sh/letsencrypt.sh -c
-rm certs.tgz; rm certs.tgz.cpt; tar zcf certs.tgz certs
+rm certs.tgz > /dev/null 2>&1; rm certs.tgz.cpt; tar zcf certs.tgz certs
 if [ -f /var/ssl/ccryptkey ]; then
   ccencrypt -K `cat ccryptkey` certs.tgz
 fi
